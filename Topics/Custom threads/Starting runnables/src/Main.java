7e68425a5c3c8500deff5549bf2df8f7ac007b20
Vincent  VAUBAN <vvauban@gmail.com>
2022-03-19 21:49:11 +0100

    Delete Topics directory

--- Topics/Custom threads/Starting runnables/src/Main.java
+++
@@ -1,9 +0,0 @@
-class Starter {
-
-    public static void startRunnables(Runnable[] runnables) {
-        // implement the method
-        for(Runnable r : runnables) {
-            new Thread(r).start();
-        }
-    }
-}
